041130f33bfb8f56b33f743da7c4d38000977b9c
Gerry Said <gerrysaid@gmail.com>
2013-02-19 16:59:13 +0100

    test

--- Assets/CubeController.js
+++ Assets/CubeController.js
@@ -10,6 +10,7 @@ function Start () {
 function Update () {
 	
 	//move according to the horizontal arrow keys and the speed
+	
 	transform.Translate(Vector3.left * speed * Input.GetAxis("Horizontal"));
 	
 }
